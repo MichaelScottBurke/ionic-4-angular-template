042830402ffe39437d5743cf0a356ec99a37f14f
MichaelScottBurke <micky68@gmail.com>
2019-02-13 15:40:20 -0500

    added lists page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'lists', loadChildren: './lists/lists.module#ListsPageModule' }
 ];
 
 @NgModule({
