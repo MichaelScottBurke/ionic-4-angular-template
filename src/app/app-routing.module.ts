4f630c90be4c0026d85d519964bd93041a0aaebd
MichaelScottBurke <micky68@gmail.com>
2019-03-08 15:18:21 -0500

    removed test iframe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -49,7 +49,6 @@ const routes: Routes = [
   { path: 'modal-example', loadChildren: './examples/modal-example/modal-example.module#ModalExamplePageModule' },
   { path: 'modal-layout', loadChildren: './modal-layout/modal-layout.module#ModalLayoutPageModule' },
   { path: 'form-grid-example', loadChildren: './examples/form-grid-example/form-grid-example.module#FormGridExamplePageModule' },
-  { path: 'test-iframe', loadChildren: './assets/templates/test-iframe/test-iframe.module#TestIframePageModule' },
   { path: 'detail-view', loadChildren: './examples/detail-view/detail-view.module#DetailViewPageModule' },
   { path: 'detail-view-example', loadChildren: './examples/detail-view-example/detail-view-example.module#DetailViewExamplePageModule' },
   { path: 'form-example-two', loadChildren: './examples/form-example-two/form-example-two.module#FormExampleTwoPageModule' },
